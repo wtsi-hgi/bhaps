387ac4ecd9bbf757510e6b641c2f7278550338b4
Joshua Randall <joshua.randall@sanger.ac.uk>
2013-11-18 11:46:18 +0000

    Fixed run_tests.sh so it can be run from anywhere.  Added Travis CI build configuration.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
+testdir=$(dirname "$(readlink -f "$0")")
+echo -n "entering: "
+pushd ${testdir} || (echo "could not find testdir: ${testdir}" && exit 1)
+
+failure=0
+
+function fail(){
+    echo "fail"
+    failure=1
+}
+
+function ok(){
+    echo "ok"
+}
+
 test=1
 echo -n "[${test}] testing haps->bhaps->haps conversion... "
 rm -f test.${test}.*
 ../haps2bhaps.pl --input test.haps --output test.${test}.bhaps --nhaps 8
 ../bhaps2haps.pl --input test.${test}.bhaps --output test.${test}.haps
-diff test.haps test.${test}.haps && echo "ok" && rm test.${test}.* || echo "fail"
+diff test.haps test.${test}.haps && ok && rm test.${test}.* || fail
 
 test=2
 echo -n "[${test}] testing haps->bhaps->haps conversion (extra_sep)... "
 rm -f test.${test}.*
 ../haps2bhaps.pl --input test_extra_space.haps --output test.${test}.bhaps --nhaps 8
 ../bhaps2haps.pl --input test.${test}.bhaps --output test.${test}.haps --extra_sep
-diff test_extra_space.haps test.${test}.haps && echo "ok" && rm test.${test}.* || echo "fail"
+diff test_extra_space.haps test.${test}.haps && ok && rm test.${test}.* || fail
 
 test=3
 echo -n "[${test}] testing sample_haps against awk standard... "
 rm -f test.${test}.*
 ../sample_haps.pl --n 2 --input test_extra_space.haps --output test.${test}.geno --output_indices test.${test}.indices --extra_sep
 ./add-haplotypes-awk.sh test_extra_space.haps test.${test}.indices > test.${test}.awk.geno
-diff test.${test}.awk.geno test.${test}.geno && echo "ok" && rm test.${test}.* || echo "fail"
+diff test.${test}.awk.geno test.${test}.geno && ok && rm test.${test}.* || fail
 
 test=4
 echo -n "[${test}] testing sample_bhaps against awk standard... "
@@ -27,7 +42,7 @@ rm -f test.${test}.*
 ../haps2bhaps.pl --input test_extra_space.haps --output test.${test}.bhaps --nhaps 8
 ../sample_bhaps.pl --n 2 --input test.${test}.bhaps --output test.${test}.geno --output_indices test.${test}.indices --extra_sep
 ./add-haplotypes-awk.sh test_extra_space.haps test.${test}.indices > test.${test}.awk.geno
-diff test.${test}.awk.geno test.${test}.geno && echo "ok" && rm test.${test}.* || echo "fail"
+diff test.${test}.awk.geno test.${test}.geno && ok && rm test.${test}.* || fail
 
 test=5
 echo -n "[${test}] testing that sample_bhaps and sample_haps produce the same indices for given seed... "
@@ -35,7 +50,7 @@ rm -f test.${test}.*
 ../haps2bhaps.pl --input test.haps --output test.${test}.bhaps --nhaps 8
 ../sample_haps.pl --n 2 --input test.haps --output test.${test}.haps.geno --output_indices test.${test}.haps.indices --extra_sep --seed 314159
 ../sample_bhaps.pl --n 2 --input test.${test}.bhaps --output test.${test}.bhaps.geno --output_indices test.${test}.bhaps.indices --extra_sep --seed 314159
-diff test.${test}.haps.indices test.${test}.bhaps.indices && echo "ok" && rm test.${test}.* || echo "fail"
+diff test.${test}.haps.indices test.${test}.bhaps.indices && ok && rm test.${test}.* || fail
 
 test=6
 echo -n "[${test}] testing that sample_bhaps and sample_haps produce the same geno file for given seed... "
@@ -43,7 +58,7 @@ rm -f test.${test}.*
 ../haps2bhaps.pl --input test.haps --output test.${test}.bhaps --nhaps 8
 ../sample_haps.pl --n 2 --input test.haps --output test.${test}.haps.geno --output_indices test.${test}.haps.indices --extra_sep --seed 314159
 ../sample_bhaps.pl --n 2 --input test.${test}.bhaps --output test.${test}.bhaps.geno --output_indices test.${test}.bhaps.indices --extra_sep --seed 314159
-diff test.${test}.haps.geno test.${test}.bhaps.geno && echo "ok" && rm test.${test}.* || echo "fail"
+diff test.${test}.haps.geno test.${test}.bhaps.geno && ok && rm test.${test}.* || fail
 
 test=7
 echo -n "[${test}] testing haps->bhaps(gz)->haps(gz)->bhaps->haps(gz)->bhaps(gz)->haps conversion... "
@@ -54,7 +69,7 @@ rm -f test.${test}.*
 ../bhaps2haps.pl --input test.${test}.bhaps3 --output test.${test}.haps4.gz
 ../haps2bhaps.pl --input test.${test}.haps4.gz --output test.${test}.bhaps5.gz --nhaps 8
 ../bhaps2haps.pl --input test.${test}.bhaps5.gz --output test.${test}.haps6
-diff test.haps test.${test}.haps6 && echo "ok" && rm test.${test}.* || echo "fail"
+diff test.haps test.${test}.haps6 && ok && rm test.${test}.* || fail
 
 test=8
 echo -n "[${test}] testing sample_haps against awk standard using gzip compression... "
@@ -62,7 +77,7 @@ rm -f test.${test}.*
 gzip -c test_extra_space.haps > test.${test}.haps.gz
 ../sample_haps.pl --n 2 --input test.${test}.haps.gz --output test.${test}.geno --output_indices test.${test}.indices --extra_sep
 ./add-haplotypes-awk.sh test_extra_space.haps test.${test}.indices > test.${test}.awk.geno
-diff test.${test}.awk.geno test.${test}.geno && echo "ok" && rm test.${test}.* || echo "fail"
+diff test.${test}.awk.geno test.${test}.geno && ok && rm test.${test}.* || fail
 
 test=9
 echo -n "[${test}] testing sample_bhaps against awk standard using gzip compression... "
@@ -70,7 +85,7 @@ rm -f test.${test}.*
 ../haps2bhaps.pl --input test_extra_space.haps --output test.${test}.bhaps.gz --nhaps 8
 ../sample_bhaps.pl --n 2 --input test.${test}.bhaps.gz --output test.${test}.geno --output_indices test.${test}.indices --extra_sep
 ./add-haplotypes-awk.sh test_extra_space.haps test.${test}.indices > test.${test}.awk.geno
-diff test.${test}.awk.geno test.${test}.geno && echo "ok" && rm test.${test}.* || echo "fail"
+diff test.${test}.awk.geno test.${test}.geno && ok && rm test.${test}.* || fail
 
 test=10
 echo -n "[${test}] testing that sample_bhaps and sample_haps produce the same indices for given seed with replacement... "
@@ -78,7 +93,7 @@ rm -f test.${test}.*
 ../haps2bhaps.pl --input test.haps --output test.${test}.bhaps --nhaps 8
 ../sample_haps.pl --n 2 --input test.haps --output test.${test}.haps.geno --output_indices test.${test}.haps.indices --extra_sep --seed 314159 --with_replacement
 ../sample_bhaps.pl --n 2 --input test.${test}.bhaps --output test.${test}.bhaps.geno --output_indices test.${test}.bhaps.indices --extra_sep --seed 314159 --with_replacement
-diff test.${test}.haps.indices test.${test}.bhaps.indices && echo "ok" && rm test.${test}.* || echo "fail"
+diff test.${test}.haps.indices test.${test}.bhaps.indices && ok && rm test.${test}.* || fail
 
 test=11
 echo -n "[${test}] testing that sample_bhaps and sample_haps produce the same geno file for given seed with replacement... "
@@ -86,7 +101,7 @@ rm -f test.${test}.*
 ../haps2bhaps.pl --input test.haps --output test.${test}.bhaps --nhaps 8
 ../sample_haps.pl --n 2 --input test.haps --output test.${test}.haps.geno --output_indices test.${test}.haps.indices --extra_sep --seed 314159 --with_replacement
 ../sample_bhaps.pl --n 2 --input test.${test}.bhaps --output test.${test}.bhaps.geno --output_indices test.${test}.bhaps.indices --extra_sep --seed 314159 --with_replacement
-diff test.${test}.haps.geno test.${test}.bhaps.geno && echo "ok" && rm test.${test}.* || echo "fail"
+diff test.${test}.haps.geno test.${test}.bhaps.geno && ok && rm test.${test}.* || fail
 
 test=12
 echo -n "[${test}] testing that sample_bhaps indices without replacement do not contain repeat indices... "
@@ -94,8 +109,12 @@ rm -f test.${test}.*
 ../sample_haps.pl --n 2 --input test.haps --output test.${test}.haps.geno --output_indices test.${test}.haps.indices --extra_sep --seed 314159
 if [ -z $(cat test.${test}.haps.indices | perl -pi -e 's/\ /\n/g' | sort | uniq -d) ]
 then 
-    echo "ok" && rm test.${test}.*
+    ok && rm test.${test}.*
 else
-    echo "fail"
+    fail
 fi
 
+echo -n "all tests complete, returning to: "
+popd
+
+exit ${failure}
